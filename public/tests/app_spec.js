285b387eec7d40795131e1f5f689d45f132601ae
yadoroku <zangyaku.fighter@gmail.com>
2018-05-13 15:12:30 +0900

    P31:modified test code(step1).

--- public/tests/app_spec.js
+++ public/tests/app_spec.js
@@ -1,5 +1,7 @@
 describe('LearnJS', function(){
 	it('can show a problem view', function(){
+		learnjs.showView('#problem-view');
+		expect($('.view-container .problem-view').length).toEqual(1);
 	});
 });
 
